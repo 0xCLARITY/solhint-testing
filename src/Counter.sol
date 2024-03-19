4f2b98017ee02434a68888450b76048617cd0b5f
Hans Bergren (0xCLARITY) <hansbergren@gmail.com>
2024-03-19 08:57:39 -0700

    Solhint bug

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
-contract Counter {
+import {ICounter} from "./ICounter.sol";
+
+contract Counter is ICounter {
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
+
+        emit Increment(msg.sender, newNumber);
     }
 
     function increment() public {
         number++;
+
+        emit Increment(msg.sender, number);
     }
 }
---
+++ src/ICounter.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.8.20;
+
+interface ICounter {
+  event Increment (address indexed sender, uint256 indexed value);
+}
